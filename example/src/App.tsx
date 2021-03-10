e4fba19b6fa3679c396d26ab518e6a74fc54b215
Nicola Sedda <nicola.sedda@qframe.be>
2021-03-11 00:32:57 +0100

    working on documentation

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { NavigationBar } from './components/NavigationBar';
+
+export const App = () => {
+    return (
+        <div className='App'>
+            <NavigationBar />
+        </div>
+    );
+};
